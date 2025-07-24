2a932f61ef11e5a49288f545c179ae5e0f82cb17
Fahmi Zain <fahmizain000@gmail.com>
2025-07-24 15:23:49 +0700

    ai connection test

--- app/api/ai/route.js
+++ app/api/ai/route.js
@@ -1,1 +1,18 @@
-// here
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextServer } from "next/server";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+export async function GET() {
+  const prompt = "Halo sebutkan provinsi di pulau jawa, kirimkan response nya dalam bentuk json ya";
+
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+  const result = await model.generateContent(prompt) 
+  const response = await result.response
+
+  const text = response.text()
+
+
+  return Response.json({text})
+}
